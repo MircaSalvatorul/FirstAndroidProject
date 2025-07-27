c23bea9f825aded49a926e86e1d5114d9f139ba4
MircaSalvatorul <mateimircea919@gmail.com>
2025-07-27 17:51:27 +0300

    Added background color

--- app/src/main/java/com/example/firstandroidgame/MainActivity.kt
+++ app/src/main/java/com/example/firstandroidgame/MainActivity.kt
@@ -7,9 +7,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.firstandroidgame.ui.theme.FirstandroidgameTheme
 
@@ -32,16 +34,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(color = Color.Magenta) {
+        Text(
+            text = "Hello, my name is $name!",
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     FirstandroidgameTheme {
-        Greeting("Android")
+        Greeting("Mircea")
     }
 }
